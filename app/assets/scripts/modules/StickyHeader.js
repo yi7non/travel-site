1fd7b3fe3c9437eb489d153dcc6c5f8b5fe515c0
yinon <yi7non@gmail.com>
2019-01-06 00:52:14 +0200

    completed sticky header functionality

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,66 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smootScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = $('.site-header');
+        this.triggerElement = $('.large-hero__title');
+        this.createHeaderWaypoint();
+        this.pageSection = $('.page-section');
+        this.headerLinks = $(".primary-nav a");
+        this.createPageSectionWaypoints();
+        this.addSmoothScrolling();
+    }
+
+    addSmoothScrolling() {
+        this.headerLinks.smootScroll();
+    }
+
+    createHeaderWaypoint() {
+        var self = this;
+        new Waypoint({
+            element:self.triggerElement[0],
+            handler: function(dir) {
+                if(dir == 'down') {
+                    self.siteHeader.addClass('site-header--dark');
+                } else {
+                    self.siteHeader.removeClass('site-header--dark');
+                }
+            }
+        });
+    }
+
+    createPageSectionWaypoints() {
+        var self = this;
+        this.pageSection.each(function() {
+            var currentPageSection = this;
+            var offset = "";
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if(direction == "down") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        self.headerLinks.removeClass('is-current-link');
+                        $(matchingHeaderLink).addClass('is-current-link');
+                    }
+                },
+                offset: '18%'
+            });
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if(direction == "up") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        self.headerLinks.removeClass('is-current-link');
+                        $(matchingHeaderLink).addClass('is-current-link');
+                    }
+                },
+                offset: '-40%'
+            });
+        });
+    }
+
+}
+
+export default StickyHeader;
